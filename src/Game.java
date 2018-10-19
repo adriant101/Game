d466c5b9b80a033ddb394871edcd3b58abd67d90
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-10-19 09:52:50 -0600

    Last push for lab 7

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,20 @@
 
 public class Game {
+private String playerName;
+private int score;
+private int highScore;
+public Game(String playerName, int score, int highScore) {
 
+	this.playerName = playerName;
+	this.score = score;
+	this.highScore = highScore;
+}
+
+public void collectScores() {
+	
+}
+
+public String toString() {
+	return "";
+}
 }
--- src/Items.java
+++ src/Items.java
@@ -4,18 +4,26 @@ public class Items {
 	private int xLocation;
 	private int yLocation;
 	private String imagePath;
-	private int itemNod;
+	private int itemQty;
 	
 	
 	
 	//Constructor
-	public Items(int xLocation, int yLocation, String imagePath) {
+	public Items(int xLocation, int yLocation, String imagePath, int itemQty) {
 		
 		this.xLocation = xLocation;
 		this.yLocation = yLocation;
 		this.imagePath = imagePath;
+		this.itemQty = itemQty;
 	}
 	
+	public void nextLevelQty() {
+		
+	}
+	
+	public void raiseScore() {
+		
+	}
 	
 	
 }
