f6c881d48f92eb3d1ce28c9546bc23fde8c0c116
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-10-19 09:31:50 -0600

    Items push

--- src/Items.java
+++ src/Items.java
@@ -1,4 +1,21 @@
 
 public class Items {
 
+	private int xLocation;
+	private int yLocation;
+	private String imagePath;
+	private int itemNo;
+	
+	
+	
+	//Constructor
+	public Items(int xLocation, int yLocation, String imagePath) {
+		
+		this.xLocation = xLocation;
+		this.yLocation = yLocation;
+		this.imagePath = imagePath;
+	}
+	
+	
+	
 }
