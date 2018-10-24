f0725858c7c78b7dc7cd6a4e440f83308c624c8c
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-10-24 17:45:01 -0600

    WASD

--- src/Movement.java
+++ src/Movement.java
@@ -21,6 +21,26 @@ public class Movement {
 		xcord = xcord+1;
 		return xcord;
     }
+	
+	//WASD 
+	public void WASD(String userKey) {
+		if (userKey.equalsIgnoreCase("w"))
+				{
+			moveup(10);
+				}
+		else if(userKey.equalsIgnoreCase("a")) {
+			moveleft(10);
+		}
+		else if (userKey.equalsIgnoreCase("s")) {
+			movedown(10);
+			
+		}
+		else if (userKey.equalsIgnoreCase("d")) {
+			moveright(10);
+		}
+	}
+	
+	
 //	public boolean enemycollisiondetection(int enemyxcord, int enemyycord, int playerxcord, int playerycord) {
 //		if (enemyxcord == playerxcord && enemyycord == playerycord) {
 //			return true;
