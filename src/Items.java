7cd10fe86421bd10d74a074b85a8a80190870837
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-10-25 11:16:07 -0600

    Starting to work on items

--- src/Items.java
+++ src/Items.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Items {
 
@@ -6,24 +7,25 @@ public class Items {
 	private String imagePath;
 	private int itemQty;
 	
+	/* Generate an array of items, which then populates the scene by random index*/
 	
-	
+	Random rand = new Random();
 	//Constructor
 	public Items(int xLocation, int yLocation, String imagePath, int itemQty) {
 		
-		this.xLocation = xLocation;
-		this.yLocation = yLocation;
+		this.xLocation = rand.nextInt(900)+10;
+		this.yLocation = rand.nextInt(900)+10;
 		this.imagePath = imagePath;
-		this.itemQty = itemQty;
 	}
 	
-	public void nextLevelQty() {
+	/*public void nextLevelQty() {
+		itemQty +=2;
 		
 	}
 	
 	public void raiseScore() {
 		
 	}
-	
+	*/
 	
 }
