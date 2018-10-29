b0b09bfa7a7715580b88c556dcbbdd02e0d3bb59
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-10-29 09:12:06 -0600

    drawing rectangle

--- src/Movement.java
+++ src/Movement.java
@@ -1,13 +1,34 @@
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Rectangle;
 
 public class Movement {
 private int Speed;
+private int x;
+private int y;
+private int width;
+private int height;
+
 
 public Movement (int Speed) {
 	this.Speed = Speed;
 }
+public Movement (int x, int y, int width, int height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	
+}
+
+
+//drawing rectangle
+public Rectangle getBounds() {
+	return new Rectangle(x, y, width, height);
+}
+
+
 	public int moveup(int ycord) {
 		ycord = ycord - Speed ;
 		return ycord;
@@ -27,7 +48,7 @@ public Movement (int Speed) {
 		xcord = xcord + Speed;
 		return xcord;
     }
-	
+
 	
 	
 	
