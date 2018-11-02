97d4514ab9396acc0ca194243f2278320fb125ee
adriant101 <42621872+adriant101@users.noreply.github.com>
2018-11-01 18:29:04 -0600

    Background at startup

--- src/GraphicsDriver.java
+++ src/GraphicsDriver.java
@@ -12,6 +12,12 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -35,12 +41,12 @@ public class GraphicsDriver extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		timer1 = new Timer(100, new myTimeHandler());
+		//timer1 = new Timer(100, new myTimeHandler());
 		this.primaryStage = primaryStage;
-		timer1.start();
+		//timer1.start();
 		game1 = new Game(Mainscene);
 		
-		TextField playername = new TextField("Enter player name");
+		TextField playername = new TextField();playername.setPromptText("Enter player name");playername.setLayoutX(200);playername.setLayoutY(150);
 		Button start1 = new Button("Start as chicken");
 
 		start1.setOnAction(new EventHandler<ActionEvent>(){
@@ -53,15 +59,17 @@ public class GraphicsDriver extends Application {
 		});
 		start1.setLayoutX(200);
 		start1.setLayoutY(200);
-		
-		Group startgroup = new Group(start1, playername);
+		Image background = new Image("file:src\\bg.jpg");
+		BackgroundImage bgimg = new BackgroundImage(background, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+		StackPane layout = new StackPane();
+		layout.setBackground(new Background(bgimg));
+        layout.setPrefSize(1024, 702); 
+		Group startgroup = new Group(layout, start1, playername);
 		Scene startscene = new Scene(startgroup, 1024, 765);
-		
-		
 		primaryStage.setScene(startscene);
         primaryStage.setTitle("Lab 7");
-        Scene newscene = null;
-        game2 = new Game(newscene);
+        /*Scene newscene = null;
+        game2 = new Game(newscene);*/
         
 		
         primaryStage.show();
@@ -70,7 +78,7 @@ public class GraphicsDriver extends Application {
         
        
 	}
-	public class myTimeHandler implements ActionListener {
+	/*public class myTimeHandler implements ActionListener {
 
 		@Override
 		public void actionPerformed(java.awt.event.ActionEvent arg0) {
@@ -82,5 +90,5 @@ public class GraphicsDriver extends Application {
 			}
 		}
 		
-	}}
+	}*/}
 
